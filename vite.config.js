eedc88099c5b8f1c4a800fcad6fe98ec52631162
Memo Chou <memochou1993@gmail.com>
2023-03-03 03:01:38 +0800

    Add deploy script

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production'
+    ? '/gpt-prompt-trainer/'
+    : '/',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
